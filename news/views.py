b68315480ed489020d6b6a3fa83ac35c495b1824
foogunlana <foogunlana@yahoo.com>
2015-03-01 18:28:38 +0000

    Only Editor account can view stears news home page and article pages

--- news/views.py
+++ news/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from stears.permissions import editor
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
 
+@user_passes_test(lambda u: editor(u), login_url='/stears/noaccess/')
 def home(request):
     context = {"bla": "hello my name is bode"}
     return render(request, 'news/home.html', context)
 
 
+@user_passes_test(lambda u: editor(u), login_url='/stears/noaccess/')
 def article(request):
     context = {"bla": "hello my name is bode"}
     return render(request, 'news/article.html', context)
