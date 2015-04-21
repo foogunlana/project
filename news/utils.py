549b7d3068e59619869263fd7187be7c6b2f2454
foogunlana <foogunlana@yahoo.com>
2015-04-21 18:07:22 +0100

    Corrected news home page by removing blue highlight anf including omitted sections

--- news/utils.py
+++ news/utils.py
@@ -11,26 +11,19 @@ class StearsPage(object):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, active, *args, **kwargs):
-        assert(type(active)==bool)
-        self.active = active
-
-    @abstractmethod
-    def is_active(self):
-        return False
-
     @abstractmethod
     def is_ready(self):
         return False
+
 
 class Article(object):
     """
      An article object to create articles with
     """
 
-    def __init__(self,headline,body):
-        assert(type(headline)==str)
-        assert(type(body)==str)
+    def __init__(self, headline, body):
+        assert(type(headline) == str)
+        assert(type(body) == str)
         self.headline = headline
         self.body = body
 
@@ -39,23 +32,21 @@ class HomePage(StearsPage):
 
     def __init__(self, *args, **kwargs):
         super(HomePage, self).__init__(*args, **kwargs)
-        self.main_feature = kwargs.get('main_feature',None)
-        self.market_snapshot = kwargs.get('market_snapshot',None)
-        self.daily_column = kwargs.get('daily_column',None)
-        self.secondary = kwargs.get('secondary',None)
-        self.tertiaries = kwargs.get('tertiaries',None)
-        self.quote = kwargs.get('quote',None)
-
-    def is_active(self):
-        return self.active
+        self.main_feature = kwargs.get('main_feature', None)
+        self.market_snapshot = kwargs.get('market_snapshot', None)
+        self.daily_column = kwargs.get('daily_column', None)
+        self.secondary = kwargs.get('secondary', None)
+        self.tertiaries = kwargs.get('tertiaries', None)
+        self.quote = kwargs.get('quote', None)
 
     def is_ready(self):
         return not self.vacancies()
 
     def vacancies(self):
         vacancies = []
-        for key,value in self.__dict__.items():
-            if value == None: vacancies = vacancies + [key]
+        for key, value in self.__dict__.items():
+            if value is None:
+                vacancies = vacancies + [key]
         return vacancies
 
 
@@ -63,28 +54,28 @@ class BusinessPage(StearsPage):
 
     def __init__(self, *args, **kwargs):
         super(BusinessPage, self).__init__(*args, **kwargs)
-        self.main_feature = kwargs.get('main_feature',None)
-        self.business_posts = kwargs.get('business_posts',None)
-
-    def is_active(self):
-        return self.active
+        self.main_feature = kwargs.get('main_feature', None)
+        self.business_posts = kwargs.get('business_posts', None)
 
     def is_ready(self):
         return False
 
 
 class EconomyPage(StearsPage):
-    
     def __init__(self, *args, **kwargs):
         super(EconomyPage, self).__init__(*args, **kwargs)
-        self.main_feature = kwargs.get('main_feature',None)
-        self.economic_snapshot = kwargs.get('economic_snapshot',None)
-        self.economy_posts = kwargs.get('economy_posts',None)
-
-    def is_active(self):
-        return self.active
+        self.main_feature = kwargs.get('main_feature', None)
+        self.economic_snapshot = kwargs.get('economic_snapshot', None)
+        self.economy_posts = kwargs.get('economy_posts', None)
 
     def is_ready(self):
         return False
 
 
+def obj_dict_recursive(obj):
+    obj_dict = dict(obj.__dict__)
+    for key in obj_dict:
+        item = obj_dict[key]
+        if hasattr(item, '__dict__'):
+            obj_dict[key] = obj_dict_recursive(item)
+    return obj_dict
