22c32c5c3b6838b38d2bc785a10b8842d40be66a
foogunlana <foogunlana@yahoo.com>
2015-04-24 13:15:29 +0100

    Added function to randomly populate home page classes for first look at the page

--- news/utils.py
+++ news/utils.py
@@ -1,4 +1,7 @@
 from abc import ABCMeta, abstractmethod
+from stears.utils import mongo_calls
+
+from stears import params
 
 
 class StearsPage(object):
@@ -21,22 +24,24 @@ class Article(object):
      An article object to create articles with
     """
 
-    def __init__(self, headline, body):
-        assert(type(headline) == str)
-        assert(type(body) == str)
+    def __init__(self, headline, content, writer, category, article_id):
         self.headline = headline
-        self.body = body
+        self.content = content
+        self.writer = writer
+        self.category = category
+        self.article_id = article_id
 
 
 class HomePage(StearsPage):
 
     def __init__(self, *args, **kwargs):
-        super(HomePage, self).__init__(*args, **kwargs)
+        super(HomePage, self).__init__()
         self.main_feature = kwargs.get('main_feature', None)
         self.market_snapshot = kwargs.get('market_snapshot', None)
         self.daily_column = kwargs.get('daily_column', None)
         self.secondary = kwargs.get('secondary', None)
         self.tertiaries = kwargs.get('tertiaries', None)
+        self.extras = kwargs.get('extras', None)
         self.quote = kwargs.get('quote', None)
 
     def is_ready(self):
@@ -53,7 +58,7 @@ class HomePage(StearsPage):
 class BusinessPage(StearsPage):
 
     def __init__(self, *args, **kwargs):
-        super(BusinessPage, self).__init__(*args, **kwargs)
+        super(BusinessPage, self).__init__()
         self.main_feature = kwargs.get('main_feature', None)
         self.business_posts = kwargs.get('business_posts', None)
 
@@ -63,7 +68,7 @@ class BusinessPage(StearsPage):
 
 class EconomyPage(StearsPage):
     def __init__(self, *args, **kwargs):
-        super(EconomyPage, self).__init__(*args, **kwargs)
+        super(EconomyPage, self).__init__()
         self.main_feature = kwargs.get('main_feature', None)
         self.economic_snapshot = kwargs.get('economic_snapshot', None)
         self.economy_posts = kwargs.get('economy_posts', None)
@@ -78,4 +83,55 @@ def obj_dict_recursive(obj):
         item = obj_dict[key]
         if hasattr(item, '__dict__'):
             obj_dict[key] = obj_dict_recursive(item)
+        elif type(item) == list:
+            elements = []
+            for element in item:
+                if hasattr(element, '__dict__'):
+                    elements.append(obj_dict_recursive(element))
+                else:
+                    elements.append(element)
+            obj_dict[key] = elements
     return obj_dict
+
+
+def random_home(collection):
+    # ids = [12, 17, 21, 27, 5, 22, 23]
+    article_collection = mongo_calls(collection)
+    items = {
+        'headline': 1,
+        'content': 1,
+        'writer': 1,
+        'category': 1,
+        'article_id': 1,
+        '_id': 0
+    }
+    articles = [article for article in article_collection.find({
+        '$query': {},
+        '$orderby': {'time': -1}}, items).limit(7)]
+
+    home_page = HomePage(
+        main_feature=Article(**articles.pop()),
+        daily_column=Article(**articles.pop()),
+        secondary=Article(**articles.pop()),
+        tertiaries=[
+            Article(**articles.pop()),
+            Article(**articles.pop())],
+        extras=[
+            Article(**articles.pop()),
+            Article(**articles.pop())],
+        quote="'If what happened in 2011 should again \
+        happen in 2015, by the grace of God, the dog and \
+        the baboon will all be soaked in blood.' - Buhari")
+
+    return home_page
+
+
+def populate_home():
+    return
+    page = random_home('migrations')
+    page_dict = obj_dict_recursive(page)
+    page_dict['active'] = True
+    page_dict['page'] = 'home'
+    onsite = mongo_calls('onsite')
+    onsite.update({'active': True},
+                  page_dict, True, False)
--- news/views.py
+++ news/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 from stears.permissions import editor
+from stears.utils import mongo_calls
 from django.contrib.auth.decorators import user_passes_test
 from utils import HomePage, BusinessPage, EconomyPage, \
 Article
 
 # Create your views here.
 
-
-
 @user_passes_test(lambda u: editor(u), login_url='/weal/')
 def article(request):
     return render(request, 'news/article.html')
 
-@user_passes_test(lambda u: editor(u), login_url='/weal/')
-def business(request):
-	return render(request, 'news/business.html')
 
 @user_passes_test(lambda u: editor(u), login_url='/weal/')
-def index(request):
-	return render(request, 'news/index.html')
+def business(request):
+    return render(request, 'news/business.html')
+
 
 @user_passes_test(lambda u: editor(u), login_url='/weal/')
 def economy(request):
-	return render(request, 'news/economy.html')
+    return render(request, 'news/economy.html')
+
+
+@user_passes_test(lambda u: editor(u), login_url='/weal/')
+def index(request):
+    onsite = mongo_calls('onsite')
+    home_page = onsite.find_one({'page': 'home'})
+    context = home_page
+    return render(request, 'news/index.html', context)
