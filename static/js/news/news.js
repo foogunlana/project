0efba0f395a95e1f1febc5d106511f6723652411
foogunlana <foogunlana@yahoo.com>
2015-07-13 11:53:37 +0100

    All link blocks are divs and use javascript to open the nested anchor which holds headline text for seo

--- static/js/news/news.js
+++ static/js/news/news.js
@@ -1,6 +1,17 @@
 
 $(document).ready(function(){
   $('#Sub-Category').change(function(event){
-    window.open($('#Sub-Category').val(),'_self');
+    window.open($('#Sub-Category').val(), '_self');
+  });
+
+  $('.nest-anchor').click(function(event){
+    event.preventDefault();
+    var a = $(this).find('.target-anchor');
+    var url = a.attr('href');
+    var target = a.attr('target');
+    if(!target){
+      target = '_self';
+    }
+    window.open($(this).find('.target-anchor').attr('href'), target);
   });
 });
