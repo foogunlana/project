6e5197d4f2a3abf4de058897eacec6adea1a1de2
foogunlana <foogunlana@yahoo.com>
2015-08-17 12:59:45 +0100

    redirect favicon

--- writers/urls.py
+++ writers/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from writers import settings
 
 urlpatterns = patterns('',
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
                            include('news.urls', namespace='news')),
                        url(r'^robots\.txt$', TemplateView.as_view(
                            template_name='robots.txt')),
+                       url(r'^favicon\.ico$', RedirectView.as_view(
+                           url='/static/images/favicon.ico'))
                        )
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
